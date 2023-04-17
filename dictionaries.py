769ff87ed5eb9b0d8dcef75a63dc32876040b5e4
Afzaa Atcha <afzaaatcha25@gmail.com>
2023-04-17 17:10:58 +0100

    second commit

---
+++ dictionaries.py
@@ -0,0 +1,19 @@
+my_friend = {
+    "name": "Pippa",
+    "horses": 25,
+    "deadly weapon": "orange"
+}
+person = {
+    "name": "Guido van Rossum",
+    "nationality": "Dutch",
+    "favorite_programming_language": "Python"
+}
+
+about_me = {
+    "name": "afzaa",
+    "age": 30,
+    "Favourite animal": "cat"
+}
+print(about_me.keys()) #prints keys
+print(about_me.values())# prints values
+print(about_me.get(key["name"]))
